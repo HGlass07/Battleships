fbaf760544594bb47fd227bcecd1255191752db7
Henry <hglasspool@googlemail.com>
2024-01-04 12:36:46 +0000

    added player and computer board generation

--- run.py
+++ run.py
@@ -1,1 +1,19 @@
-print("test")
+import random
+
+def create_and_print_boards(dims):
+    player_board = [['O' for _ in range(dims)] for _ in range(dims)]
+    computer_board = [['O' for _ in range(dims)] for _ in range(dims)]
+
+    print("Player Board:")
+    for row in player_board:
+        print(*row)
+
+    print("\nComputer Board:")
+    for row in computer_board:
+        print(*row)
+
+
+def main():
+    create_and_print_boards(5)
+
+main()
