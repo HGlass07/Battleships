9fc6bb7498cbdb4b9f66c2465ea2aec4627dc813
Henry <hglasspool@googlemail.com>
2024-01-08 09:56:50 +0000

    added print board functions

--- run.py
+++ run.py
@@ -2,16 +2,17 @@ import random
 
 def instructions():
     print("Welcome to Battleships\n")
-    print("Below you will see you board, with your ships represended by 'S' symbols\n")
+    print("Below you will see your board, with your ships represended by 'S' symbols\n")
     print("Start by guessing a coordinate to try and hit your opponent's ships\n")
+    print("The game ends when either you or the computer sinks all their opponent's ships")
 
 
-"""generates player and computer opponent boards"""
+'''generates player and computer opponent boards'''
 def create_boards(dims):
     player_board = [['O' for _ in range(dims)] for _ in range(dims)]
     computer_board = [['O' for _ in range(dims)] for _ in range(dims)]
 
-    """four randomly placed 'ships', represended by 'S' are placed on the player board"""
+    '''four randomly placed 'ships', represended by 'S' are placed on the player board'''
     for _ in range(4):
         while True:
             row = random.randint(0, dims - 1)
@@ -20,7 +21,7 @@ def create_boards(dims):
                 player_board[row][col] = 'S'
                 break
 
-    """four randomly placed 'ships', represended by 'S' are placed on the computer board"""
+    '''four randomly placed 'ships', represended by 'S' are placed on the computer board'''
     for _ in range(4):
         while True:
             row = random.randint(0, dims - 1)
@@ -29,24 +30,36 @@ def create_boards(dims):
                 computer_board[row][col] = 'S'
                 break
 
-    """player board is printed at the start"""
+    '''player board is printed at the start'''
     print("Player Board:")
-    for row in player_board:
-        print(*row)
+    print_board(player_board)
 
     return player_board, computer_board
+
+'''player print board function'''
+def print_board(board):
+    for row in board:
+        print(*row)
+
+'''computer print board function - computer board is printed without showing ship location to player,
+ships are only revealed once hit, shown as an "X"'''
+def print_computer_board(computer_board):
+    hidden_board = [['O' if cell != 'X' else 'X' for cell in row] for row in computer_board]
+    print("Computer's Board:")
+    for row in hidden_board: 
+        print(*row)
+
 
 
 def play_game(dims):
     player_board, computer_board = create_boards(dims)
+    player_guessed_coordinates = set()
+    computer_guessed_coordinates = set()
 
     
-
-
 
 def main():
     instructions()
     create_boards(5)
     
-
 main()
