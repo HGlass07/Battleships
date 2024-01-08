7c75e394539037c02a6ca6557437497604174c68
Henry <hglasspool@googlemail.com>
2024-01-08 09:45:03 +0000

    altered play_game function - added board and guess variables

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import random
 
 def instructions():
     print("Welcome to Battleships\n")
-    print("Below you will see you board, with your ships represended by '*' symbols\n")
+    print("Below you will see you board, with your ships represended by 'S' symbols\n")
     print("Start by guessing a coordinate to try and hit your opponent's ships\n")
 
 
@@ -11,7 +11,7 @@ def create_boards(dims):
     player_board = [['O' for _ in range(dims)] for _ in range(dims)]
     computer_board = [['O' for _ in range(dims)] for _ in range(dims)]
 
-    """four randomly placed 'ships', represended by '*' are placed on the player board"""
+    """four randomly placed 'ships', represended by 'S' are placed on the player board"""
     for _ in range(4):
         while True:
             row = random.randint(0, dims - 1)
@@ -20,7 +20,7 @@ def create_boards(dims):
                 player_board[row][col] = 'S'
                 break
 
-    """four randomly placed 'ships', represended by '*' are placed on the computer board"""
+    """four randomly placed 'ships', represended by 'S' are placed on the computer board"""
     for _ in range(4):
         while True:
             row = random.randint(0, dims - 1)
@@ -37,24 +37,10 @@ def create_boards(dims):
     return player_board, computer_board
 
 
-def play_game(player_board, computer_board):
-    x_coord = input("Please select an x coordinate between 0 and 4")
-    y_coord = input("Please select an x coordinate between 0 and 4")
-    player_guess = x_coord + y_coord
+def play_game(dims):
+    player_board, computer_board = create_boards(dims)
 
-    if player_guess == 'S' in computer_board:
-        print("Hit!")
-    else:
-        print("You missed")
-   
-    comp_x_coord = random.randint(0, 4)
-    comp_y_coord = random.randint(0, 4)
-    computer_guess = comp_x_coord + comp_y_coord
-
-    if computer_guess == 'S' in player_board:
-        print("You've been hit!")
-    else:
-        print("Your opponent missed")
+    
 
 
 
