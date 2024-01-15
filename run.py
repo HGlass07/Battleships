35eeb5018ae18762fc27520beabd08c7927886fe
Henry <hglasspool@googlemail.com>
2024-01-15 14:54:43 +0000

    adjusted game function - added 'self' perameters

--- run.py
+++ run.py
@@ -54,15 +54,13 @@ class Battleships:
 
 
     # coordinate check function ensures coodinate guesses are within valid range
-    def valid_coord(dims, row, col):
-        return 0 <= row < dims and 0 <= col < dims
+    def valid_coord(self, row, col):
+        return 0 <= row < self.dims and 0 <= col < self.dims
 
 
     # main game function
-    def play_game(dims):
-        player_board, computer_board = create_boards(dims)
-        player_guess = set()
-        computer_guess = set()
+    def play_game(self):
+        self.create_boards()
 
         while True:
             # player turn
@@ -71,16 +69,16 @@ class Battleships:
                 try:
                     player_row_guess = int(input("Enter a row number: "))
                     player_col_guess = int(input("Enter a column number: "))
-                    if not valid_coord(dims, player_row_guess, player_col_guess):
+                    if not self.valid_coord(player_row_guess, player_col_guess):
                         print("Invalid coordinates, please guess again.")
                         continue
                     # checks if player has already guessed coordinates
-                    if (player_row_guess, player_col_guess) in player_guess:
+                    if (player_row_guess, player_col_guess) in self.player_guess:
                         print("You've already guessed these coordinates") 
                         print("please guess again.\n")
                         continue
 
-                    player_guess.add((player_row_guess, player_col_guess))
+                    self.player_guess.add((player_row_guess, player_col_guess))
                     break
 
                 except ValueError:
@@ -88,17 +86,17 @@ class Battleships:
                     continue
 
             # checks if player guess hits opponent ship
-            if computer_board[player_row_guess][player_col_guess] == 'S':
+            if self.computer_board[player_row_guess][player_col_guess] == 'S':
                 print("Hit!")
-                computer_board[player_row_guess][player_col_guess] = 'X'
+                self.computer_board[player_row_guess][player_col_guess] = 'X'
             else:
                 print("You missed")
 
             # prints updated computer board, only hit ships will show
-            print_computer_board(computer_board)
+            self.print_computer_board()
 
             # checks for player win
-            if all('S' not in row for row in computer_board):
+            if all('S' not in row for row in self.computer_board):
                 print("Well done! You sunk all your opponent's ships") 
                 print("and won the game\n")
                 break
@@ -106,33 +104,34 @@ class Battleships:
             # computer turn
             print("\nComputer's Turn")
             while True:
-                computer_row_guess = random.randint(0, dims - 1)
-                computer_col_guess = random.randint(0, dims - 1)
+                computer_row_guess = random.randint(0, self.dims - 1)
+                computer_col_guess = random.randint(0, self.dims - 1)
 
                 # checks if computer has already guessed coordinates
-                if (computer_row_guess, computer_col_guess) not in computer_guess:
-                    computer_guess.add((computer_row_guess, computer_col_guess))
+                if (computer_row_guess, computer_col_guess) not in self.computer_guess:
+                    self.computer_guess.add((computer_row_guess, computer_col_guess))
                     break
+
             # checks if computer guess hits player ship
-            if player_board[computer_row_guess][computer_col_guess] == 'S':
+            if self.player_board[computer_row_guess][computer_col_guess] == 'S':
                 print("Computer hit at coordinates: ({}, {})".format
                     (computer_row_guess, computer_col_guess))
-                player_board[computer_row_guess][computer_col_guess] = 'X'
+                self.player_board[computer_row_guess][computer_col_guess] = 'X'
             else:
                 print("Computer missed")
 
             # prints updated player board, updated with hits if applicable
             print("Your board: ")
-            print_board(player_board)
+            self.print_board()
 
             # checks for computer win
-            if all('S' not in row for row in player_board):
+            if all('S' not in row for row in self.player_board):
                 print("The computer sunk all your ships, you lose!\n")
                 break
 
         restart = input("Do you want to play again? (yes/no):\n ")
         if restart.lower() == 'yes':
-            play_game(dims)
+            self.play_game()
         else:
             print("Thanks for playing")
 
