1b720c4e57d9001e0dd64eb9eeae05dd2cb40bc1
Henry <hglasspool@googlemail.com>
2024-01-16 11:02:42 +0000

    text changes following new linter test

--- run.py
+++ run.py
@@ -2,16 +2,16 @@ import random
 
 
 class Battleships:
-    #class constructor
+    # class constructor
     def __init__(self, dims):
         self.dims = dims
-        self.player_board = [['O' for _ in range(dims)] 
-        for _ in range(dims)]
-        self.computer_board = [['O' for _ in range(dims)] for _ in range(dims)]
-        self.player_guess = set()
-        self.computer_guess = set()
+        self.user_board = [['O' for _ in range(dims)]
+                           for _ in range(dims)]
+        self.comp_board = [['O' for _ in range(dims)] for _ in range(dims)]
+        self.user_guess = set()
+        self.comp_guess = set()
 
-    #game instructions
+    # game instructions
     def instructions(self):
         print("\nWelcome to Battleships!\n")
         print("Below you will see your board, with your")
@@ -21,23 +21,24 @@ class Battleships:
         print("The game ends when either you or the computer sinks")
         print("all their opponent's ships.\n")
 
-
     # generates player and computer opponent boards
     # four randomly placed 'ships', represended by 'S' are
     # placed on the player and computer boards
     def create_boards(self):
 
-        self.player_board = [['O' for _ in range(self.dims)] for _ in range(self.dims)]
-        self.computer_board = [['O' for _ in range(self.dims)] for _ in range(self.dims)]
+        self.user_board = [['O' for _ in range(self.dims)]
+                           for _ in range(self.dims)]
+        self.comp_board = [['O' for _ in range(self.dims)]
+                           for _ in range(self.dims)]
 
         for _ in range(4):
-            self.place_ship(self.player_board)
+            self.place_ship(self.user_board)
 
         for _ in range(4):
-            self.place_computer_ship()
+            self.place_comp_ship()
 
         print("Player Board:")
-        self.print_board(self.player_board)
+        self.print_board(self.user_board)
 
     # prints player and computer boards
     def place_ship(self, board):
@@ -48,34 +49,32 @@ class Battleships:
                 board[row][col] = 'S'
                 break
 
-    def place_computer_ship(self):
+    def place_comp_ship(self):
         while True:
             row = random.randint(0, self.dims - 1)
             col = random.randint(0, self.dims - 1)
-            if self.computer_board[row][col] == 'O':
-                self.computer_board[row][col] = 'S'
+            if self.comp_board[row][col] == 'O':
+                self.comp_board[row][col] = 'S'
                 break
 
     def print_board(self, board):
         for row in board:
             print(*row)
 
-    def print_computer_board(self):
-        hidden_board = [['O' if cell != 'X' else 'X' for cell in row] for row in self.computer_board]
+    def print_comp_board(self):
+        hidden_board = [['O' if cell != 'X' else 'X' for cell in row]
+                        for row in self.comp_board]
         print("Computer's Board:")
-        self.print_board(hidden_board)    
+        self.print_board(hidden_board)
 
-
-    # coordinate check function ensures coodinate guesses are within valid range
-    def valid_coord(self, row, col):
+    # coordinate check function ensures guesses are within valid range
+    def val_coord(self, row, col):
         return 0 <= row < self.dims and 0 <= col < self.dims
-
 
     # main game function
     def play_game(self):
-        
-        self.player_guess = set()
-        self.computer_guess = set()
+        self.user_guess = set()
+        self.comp_guess = set()
 
         self.create_boards()
 
@@ -86,65 +85,66 @@ class Battleships:
                 try:
                     # player enteres coordinate guess
                     # guess undergoes validation
-                    player_row_guess = int(input("Enter a row number: "))
-                    player_col_guess = int(input("Enter a column number: "))
-                    if not self.valid_coord(player_row_guess, player_col_guess):
+                    user_row_guess = int(input("Enter a row number: "))
+                    user_col_guess = int(input("Enter a column number: "))
+                    if not self.val_coord(user_row_guess, user_col_guess):
                         print("Invalid coordinates, please guess again.")
                         continue
                     # checks if player has already guessed coordinates
-                    if (player_row_guess, player_col_guess) in self.player_guess:
-                        print("You've already guessed these coordinates") 
+                    if (user_row_guess, user_col_guess) in self.user_guess:
+                        print("You've already guessed these coordinates")
                         print("please guess again.\n")
                         continue
 
-                    self.player_guess.add((player_row_guess, player_col_guess))
+                    self.user_guess.add((user_row_guess, user_col_guess))
                     break
 
                 except ValueError:
-                    print("Invalid input. Please enter a valid numeric value.\n")
+                    print("Invalid input")
+                    print("Please enter a valid numeric value.\n")
                     continue
 
             # checks if player guess hits opponent ship
-            if self.computer_board[player_row_guess][player_col_guess] == 'S':
+            if self.comp_board[user_row_guess][user_col_guess] == 'S':
                 print("Hit!")
-                self.computer_board[player_row_guess][player_col_guess] = 'X'
+                self.comp_board[user_row_guess][user_col_guess] = 'X'
             else:
                 print("You missed")
 
             # prints updated computer board, only hit ships will show
-            self.print_computer_board()
+            self.print_comp_board()
 
             # checks for player win
-            if all('S' not in row for row in self.computer_board):
-                print("Well done! You sunk all your opponent's ships") 
+            if all('S' not in row for row in self.comp_board):
+                print("Well done! You sunk all your opponent's ships")
                 print("and won the game\n")
                 break
 
             # computer turn
             print("\nComputer's Turn")
             while True:
-                computer_row_guess = random.randint(0, self.dims - 1)
-                computer_col_guess = random.randint(0, self.dims - 1)
+                comp_row_guess = random.randint(0, self.dims - 1)
+                comp_col_guess = random.randint(0, self.dims - 1)
 
                 # checks if computer has already guessed coordinates
-                if (computer_row_guess, computer_col_guess) not in self.computer_guess:
-                    self.computer_guess.add((computer_row_guess, computer_col_guess))
+                if (comp_row_guess, comp_col_guess) not in self.comp_guess:
+                    self.comp_guess.add((comp_row_guess, comp_col_guess))
                     break
 
             # checks if computer guess hits player ship
-            if self.player_board[computer_row_guess][computer_col_guess] == 'S':
+            if self.user_board[comp_row_guess][comp_col_guess] == 'S':
                 print("Computer hit at coordinates: ({}, {})".format
-                    (computer_row_guess, computer_col_guess))
-                self.player_board[computer_row_guess][computer_col_guess] = 'X'
+                      (comp_row_guess, comp_col_guess))
+                self.user_board[comp_row_guess][comp_col_guess] = 'X'
             else:
                 print("Computer missed")
 
             # prints updated player board, updated with hits if applicable
             print("Your board: ")
-            self.print_board(self.player_board)
+            self.print_board(self.user_board)
 
             # checks for computer win
-            if all('S' not in row for row in self.player_board):
+            if all('S' not in row for row in self.user_board):
                 print("The computer sunk all your ships, you lose!\n")
                 break
 
@@ -159,6 +159,7 @@ class Battleships:
                 break
             else:
                 print("Invalid input. Please enter 'yes' or 'no'.")
+
 
 def main():
     game = Battleships(5)
